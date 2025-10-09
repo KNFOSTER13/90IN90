93b050cee764a7940875ec9d4c8362dc6191d96c
KNFOSTER13 <submissions@forharriet.com>
2025-10-08 22:06:13 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,17 @@
 // js/firebase-config.js
 // This file contains the shared Firebase configuration for the app.
 
+// In a local environment, you can hardcode these.
+// For production on GitHub, these values should be replaced by a CI/CD pipeline (like GitHub Actions)
+// using secrets. This placeholder setup is for demonstration.
 export const firebaseConfig = {
-    apiKey: "AIzaSyDTd4lvs1lzjKeGu7Ee91lM30-8v88h-ng",
-    authDomain: "productivity-tracker-knf13.firebaseapp.com",
+    apiKey: "%FIREBASE_API_KEY%",
+    authDomain: "%FIREBASE_AUTH_DOMAIN%",
     projectId: "productivity-tracker-knf13",
-    storageBucket: "productivity-tracker-knf13.appspot.com",
-    messagingSenderId: "762035393571",
-    appId: "1:762035393571:web:7d474a6a57cc031b860d96"
+    storageBucket: "%FIREBASE_STORAGE_BUCKET%",
+    messagingSenderId: "%FIREBASE_MESSAGING_SENDER_ID%",
+    appId: "%FIREBASE_APP_ID%"
 };
 
+// This is NOT a secret and can be public.
 export const ADMIN_EMAIL = "submissions@forharriet.com";
