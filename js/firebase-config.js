647cf13453299b6d5bdd9ee94fa60a0fe279ba22
KNFOSTER13 <submissions@forharriet.com>
2025-10-08 20:12:09 -0500

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// js/firebase-config.js
+// This file contains the shared Firebase configuration for the app.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDTd4lvs1lzjKeGu7Ee91lM30-8v88h-ng",
+    authDomain: "productivity-tracker-knf13.firebaseapp.com",
+    projectId: "productivity-tracker-knf13",
+    storageBucket: "productivity-tracker-knf13.appspot.com",
+    messagingSenderId: "762035393571",
+    appId: "1:762035393571:web:7d474a6a57cc031b860d96"
+};
+
+export const ADMIN_EMAIL = "submissions@forharriet.com";
